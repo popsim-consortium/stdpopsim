6286db0425e0931803c4a399fa748618a323491c
Graham Gower <graham.gower@gmail.com>
2020-03-28 12:32:34 +0100

    Pin pyslim>=0.401.
    This avoids an issue with recapitation, arising due to changes between
    msprime-0.7.4 and msprime-current.
    https://github.com/tskit-dev/pyslim/issues/71

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     },
     # NOTE: make sure this is the 'attrs' package, not 'attr'!
     install_requires=["msprime>=0.7.1", "attrs>=19.1.0", "appdirs", "humanize",
-                      "pyslim"],
+                      "pyslim>=0.401"],
     url='https://github.com/popsim-consortium/stdpopsim',
     project_urls={
         'Bug Reports': 'https://github.com/popsim-consortium/stdpopsim/issues',
