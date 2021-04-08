63ebecd45be1fd2bfccc8a50fc0beaf32f488985
andrewkern <adkern@uoregon.edu>
2021-04-08 09:55:07 -0700

    Anopheles gambiae added to the Catalog

---
+++ stdpopsim/catalog/AnoGam/__init__.py
@@ -0,0 +1,5 @@
+"""
+Catalog definitions for AnoGam (Ensembl ID='anopheles_gambiae')
+"""
+from . import species  # noqa: F401
+from . import demographic_models  # noqa: F401
---
+++ stdpopsim/catalog/AnoGam/species.py
@@ -0,0 +1,73 @@
+import stdpopsim
+
+from . import genome_data
+
+
+_Ag1000G = stdpopsim.Citation(
+    doi="https://doi.org/10.1038/nature24995",
+    year=2017,
+    author="Ag1000G Consortium",
+    reasons={
+        stdpopsim.CiteReason.MUT_RATE,
+        stdpopsim.CiteReason.GEN_TIME,
+        stdpopsim.CiteReason.POP_SIZE,
+    },
+)
+
+_PombiEtAl = stdpopsim.Citation(
+    doi="https://doi.org/10.4269/ajtmh.2006.75.901",
+    year=2006,
+    author="Pombi et al.",
+    reasons={stdpopsim.CiteReason.REC_RATE},
+)
+
+_SharakhovaEtAl = stdpopsim.Citation(
+    doi="https://doi.org/10.1186/gb-2007-8-1-r5",
+    year=2006,
+    author="Sharakhova et al.",
+    reasons={stdpopsim.CiteReason.ASSEMBLY},
+)
+
+_recombination_rate = {
+    "2L": 0,  # setting to zero because of inversion
+    "2R": 1.3e-8,
+    "3L": 1.6e-8,
+    "3R": 1.3e-8,
+    "X": 1e-8,
+    "Mt": 0,
+}
+
+
+_overall_rate = 5.49e-9
+_mutation_rate = {
+    "2L": _overall_rate,
+    "2R": _overall_rate,
+    "3L": _overall_rate,
+    "3R": _overall_rate,
+    "X": _overall_rate,
+    "Mt": _overall_rate,
+}
+
+_genome = stdpopsim.Genome.from_data(
+    genome_data.data,
+    recombination_rate=_recombination_rate,
+    mutation_rate=_mutation_rate,
+    citations=[
+        _SharakhovaEtAl,
+        _Ag1000G,
+        _PombiEtAl,
+    ],
+)
+
+_species = stdpopsim.Species(
+    id="AnoGam",
+    ensembl_id="anopheles_gambiae",
+    name="Anopheles gambiae",
+    common_name="Anopheles gambiae",
+    genome=_genome,
+    generation_time=1 / 11,
+    population_size=6e6,  # Ghana population
+    citations=[_Ag1000G],
+)
+
+stdpopsim.register_species(_species)
