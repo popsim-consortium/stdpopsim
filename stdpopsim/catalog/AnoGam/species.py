f14f7bc00816f80e2600882f6f7981afa859c43d
peter <petrel.harp@gmail.com>
2021-04-10 09:55:35 -0700

    QC for AnoGam
    mut rate for demographic model
    Pombi note

--- stdpopsim/catalog/AnoGam/species.py
+++ stdpopsim/catalog/AnoGam/species.py
@@ -21,6 +21,14 @@ _PombiEtAl = stdpopsim.Citation(
     reasons={stdpopsim.CiteReason.REC_RATE},
 )
 
+_ZhengEtAl = stdpopsim.Citation(
+    # there's no DOI?!?!
+    doi="https://www.genetics.org/content/143/2/941",
+    year=1996,
+    author="Zheng et al.",
+    reasons={stdpopsim.CiteReason.REC_RATE},
+)
+
 _SharakhovaEtAl = stdpopsim.Citation(
     doi="https://doi.org/10.1186/gb-2007-8-1-r5",
     year=2006,
@@ -28,17 +36,30 @@ _SharakhovaEtAl = stdpopsim.Citation(
     reasons={stdpopsim.CiteReason.ASSEMBLY},
 )
 
+_KeightleyEtAl = stdpopsim.Citation(
+    doi="https://doi.org/10.1101/gr.091231.109",
+    year=2009,
+    author="Keightley et al.",
+    reasons={stdpopsim.CiteReason.MUT_RATE},
+)
+
+# values are in Pombi et al, but except for the X,
+# they refer to Zheng et al for the data
+# and have no data for 2L so we set equal to 2R
+# the value for X is the average from Table 1 in Zheng
 _recombination_rate = {
-    "2L": 0,  # setting to zero because of inversion
+    "2L": 1.3e-8,
     "2R": 1.3e-8,
-    "3L": 1.6e-8,
-    "3R": 1.3e-8,
-    "X": 1e-8,
+    "3L": 1.3e-8,
+    "3R": 1.6e-8,
+    "X": 2.04e-8,
     "Mt": 0,
 }
 
 
-_overall_rate = 5.49e-9
+# the rate for DroMel as inferred by Keightley et al 2009,
+# which was used for the stairwayplot demographic inference
+_overall_rate = 3.5e-9
 _mutation_rate = {
     "2L": _overall_rate,
     "2R": _overall_rate,
@@ -56,6 +77,8 @@ _genome = stdpopsim.Genome.from_data(
         _SharakhovaEtAl,
         _Ag1000G,
         _PombiEtAl,
+        _KeightleyEtAl,
+        _ZhengEtAl,
     ],
 )
 stdpopsim.utils.append_common_synonyms(_genome)
@@ -67,7 +90,8 @@ _species = stdpopsim.Species(
     common_name="Anopheles gambiae",
     genome=_genome,
     generation_time=1 / 11,
-    population_size=6e6,  # Ghana population
+    # based on theta = 4 Ne u in Gabon population, rounded
+    population_size=1e6,
     citations=[_Ag1000G],
 )
 
