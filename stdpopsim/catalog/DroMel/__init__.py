4fcca0ab1ad162f8eed34ddf2c6d0e8e42c5f8bd
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2021-10-20 21:07:44 -0700

    Drosophila DFE Huber et al 2017

--- stdpopsim/catalog/DroMel/__init__.py
+++ stdpopsim/catalog/DroMel/__init__.py
@@ -4,3 +4,4 @@ Catalog data for Drosophila Melanogaster
 from . import species  # noqa: F401
 from . import genetic_maps  # noqa: F401
 from . import demographic_models  # noqa: F401
+from . import dfes  # noqa: F401
