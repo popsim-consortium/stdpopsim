2fbfc749bf9af76dd023cae132eca954a6d8b25a
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2021-10-29 13:34:46 -0700

    Update test_DroSec.py

--- tests/test_DroSec.py
+++ tests/test_DroSec.py
@@ -21,27 +21,44 @@ class TestSpeciesData(test_species.SpeciesTestBase):
     # independently referring to the citations provided in the
     # species definition, filling in the appropriate values
     # and deleting the pytest "skip" annotations.
-    @pytest.mark.skip("Population size QC not done yet")
     def test_qc_population_size(self):
-        assert self.species.population_size == -1
+        assert self.species.population_size == 100000
 
     @pytest.mark.skip("Generation time QC not done yet")
     def test_qc_generation_time(self):
-        assert self.species.generation_time == -1
+        assert self.species.generation_time == 20
 
 
 class TestGenomeData(test_species.GenomeTestBase):
 
     genome = stdpopsim.get_species("DroSec").genome
 
-    @pytest.mark.skip("Recombination rate QC not done yet")
-    @pytest.mark.parametrize(["name", "rate"], {}.items())
+    @pytest.mark.parametrize(
+        ["name", "rate"],
+        {
+            "2L": 2.39,
+            "2R": 2.66,
+            "3L": 1.79,
+            "3R": 1.96,
+            "X": 2.95,
+            "4": 0,
+        }.items(),
+    )
     def test_recombination_rate(self, name, rate):
         assert rate == pytest.approx(
             self.genome.get_chromosome(name).recombination_rate
         )
 
-    @pytest.mark.skip("Mutation rate QC not done yet")
-    @pytest.mark.parametrize(["name", "rate"], {}.items())
+    @pytest.mark.parametrize(
+        ["name", "rate"],
+        {
+            "2L": 1.5e-09,
+            "2R": 1.5e-09,
+            "3L": 1.5e-09,
+            "3R": 1.5e-09,
+            "X": 1.5e-09,
+            "4": 1.5e-09,
+        }.items(),
+    )
     def test_mutation_rate(self, name, rate):
         assert rate == pytest.approx(self.genome.get_chromosome(name).mutation_rate)
