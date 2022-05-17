88e4220964e62ada843062ede5609fe054b5d941
Vitor Pavinato <vitorpavinato@users.noreply.github.com>
2022-05-16 20:53:02 -0400

    qc for DroSec

--- tests/test_DroSec.py
+++ tests/test_DroSec.py
@@ -23,25 +23,42 @@ class TestSpeciesData(test_species.SpeciesTestBase):
     # and deleting the pytest "skip" annotations.
     @pytest.mark.skip("Population size QC not done yet")
     def test_qc_population_size(self):
-        assert self.species.population_size == -1
+        assert self.species.population_size == 1e5
 
-    @pytest.mark.skip("Generation time QC not done yet")
     def test_qc_generation_time(self):
-        assert self.species.generation_time == -1
+        assert self.species.generation_time == 1 / 20
 
 
 class TestGenomeData(test_species.GenomeTestBase):
 
     genome = stdpopsim.get_species("DroSec").genome
 
-    @pytest.mark.skip("Recombination rate QC not done yet")
-    @pytest.mark.parametrize(["name", "rate"], {}.items())
+    @pytest.mark.parametrize(
+        ["name", "rate"],
+        {
+            "2L": 2.28e-8,
+            "2R": 2.51e-8,
+            "3L": 1.88e-8,
+            "3R": 1.91e-8,
+            "4": 0,
+            "X": 2.85e-8,
+        }.items(),
+    )
     def test_recombination_rate(self, name, rate):
         assert rate == pytest.approx(
             self.genome.get_chromosome(name).recombination_rate
         )
 
-    @pytest.mark.skip("Mutation rate QC not done yet")
-    @pytest.mark.parametrize(["name", "rate"], {}.items())
+    @pytest.mark.parametrize(
+        ["name", "rate"],
+        {
+            "2L": 1.5e-9,
+            "2R": 1.5e-9,
+            "3L": 1.5e-9,
+            "3R": 1.5e-9,
+            "4": 1.5e-9,
+            "X": 1.5e-9,
+        }.items(),
+    )
     def test_mutation_rate(self, name, rate):
         assert rate == pytest.approx(self.genome.get_chromosome(name).mutation_rate)
