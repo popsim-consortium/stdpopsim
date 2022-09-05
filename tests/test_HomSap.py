ffc70b328b1516c3e3d952ac3a85f8ebc7e568cd
nspope <nspope@utexas.edu>
2022-09-04 21:11:16 -0700

    Update recombination rates for HomSap using HapMapII_GRCh38 liftover (#1345)
    * Update recombination rates for HomSap using HapMapII_GRCh38

--- tests/test_HomSap.py
+++ tests/test_HomSap.py
@@ -18,34 +18,22 @@ class TestSpecies(test_species.SpeciesTestBase):
 
 class TestGenome(test_species.GenomeTestBase):
 
-    genome = stdpopsim.get_species("HomSap").genome
+    species = stdpopsim.get_species("HomSap")
+    genome = species.genome
 
     def test_basic_attributes(self):
         assert len(self.genome.chromosomes) == 25
 
-    @pytest.mark.skip("Failing - due to mismatching assembly?")
     @pytest.mark.parametrize("chr_id", [chrom.id for chrom in genome.chromosomes])
     def test_recombination_rates(self, chr_id):
-        # We should recast this test and just hard code in the values.
-        # Tests should be *obvious* not clever.
-
-        # recompute recombination rates from HapMapII_GRCh37 map then
-        # compare the results to the current recombination rates for each chromosome
-        genetic_map = "HapMapII_GRCh37"
-        species = stdpopsim.get_species("HomSap")
-        chrom = species.genome.get_chromosome(chr_id)
+        genetic_map = "HapMapII_GRCh38"
+        chrom = self.genome.get_chromosome(chr_id)
         if chr_id in ["X", "Y", "MT"]:
             with pytest.warns(stdpopsim.NonAutosomalWarning):
-                contig = species.get_contig(chr_id, genetic_map=genetic_map)
-        elif chr_id in ["3", "5", "7", "11", "16", "17", "18", "20"]:
-            contig = species.get_contig(chr_id, genetic_map=genetic_map)
+                contig = self.species.get_contig(chr_id, genetic_map=genetic_map)
         else:
-            # The rest of the chromosomes are currently emitting a warning about
-            # the mismatch in chromosome lengths because of the fact that we're
-            # on 37 for the map. This should be resolved when we start using the
-            # lifted over map.
-            with pytest.warns(UserWarning, match="longer than chromosome length"):
-                contig = species.get_contig(chr_id, genetic_map=genetic_map)
+            contig = self.species.get_contig(chr_id, genetic_map=genetic_map)
+        assert chrom.length == contig.recombination_map.sequence_length
         assert chrom.recombination_rate == pytest.approx(
             contig.recombination_map.mean_rate, rel=1e-6
         )
