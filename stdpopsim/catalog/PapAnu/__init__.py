1ce134c5a29b4e04b94198d6593fadf425dfabd4
Saurabh Belsare <saurabhmbelsare@gmail.com>
2022-09-19 22:37:09 +0530

    Fixed the error in the genetic map -> Missing sha, incorrect file_pattern, missing import (#1340)
    Changed sha256 to match the new tarball
    
    Fixed the corrupted genetic map file
    
    Fixed name mistake for PapAnu genetic file

--- stdpopsim/catalog/PapAnu/__init__.py
+++ stdpopsim/catalog/PapAnu/__init__.py
@@ -3,3 +3,4 @@ Catalog definitions for PapAnu (Ensembl ID='papio_anubis')
 """
 from . import species  # noqa: F401
 from . import demographic_models  # noqa: F401
+from . import genetic_maps  # noqa: F401
