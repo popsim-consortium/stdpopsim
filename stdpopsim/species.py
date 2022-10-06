cd4f990eab67cbc563d63f028a53c2a8f1441a0a
Nate Pope <nspope@utexas.edu>
2022-10-06 10:08:49 -0700

    Small docstring addition

--- stdpopsim/species.py
+++ stdpopsim/species.py
@@ -65,7 +65,7 @@ def get_species(id):
 
 def all_species():
     """
-    Returns an iterator over all species in the catalog.
+    Returns an iterator over all species in the catalog sorted by ID.
     """
     for species in sorted(registered_species.keys()):
         yield registered_species[species]
