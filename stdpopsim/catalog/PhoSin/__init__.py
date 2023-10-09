57034a6622c59a0ca455fb3ec42946654d1ee112
chriscrsmith <chriscs@uoregon.edu>
2023-10-09 14:22:07 -0700

    vaquita annotations.py
    small changes
    
    md5sums updated

--- stdpopsim/catalog/PhoSin/__init__.py
+++ stdpopsim/catalog/PhoSin/__init__.py
@@ -3,3 +3,4 @@ Catalog definitions for PhoSin (Ensembl ID='phocoena_sinus')
 """
 from . import species  # noqa: F401
 from . import demographic_models  # noqa: F401
+from . import annotations  # noqa: F401
