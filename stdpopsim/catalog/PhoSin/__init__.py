2a92f239bec13ffcd21e5e2e0b739ee73003f2ae
Ilan Gronau <ilan.gronau@idc.ac.il>
2024-02-07 15:40:47 +0200

    Initial implementation for Vaquita DFE
    added dcomentation to DFE and the h-s relationship
    
    fixed import in PhoSin/__init__.py

--- stdpopsim/catalog/PhoSin/__init__.py
+++ stdpopsim/catalog/PhoSin/__init__.py
@@ -4,3 +4,4 @@ Catalog definitions for PhoSin (Ensembl ID='phocoena_sinus')
 from . import species  # noqa: F401
 from . import demographic_models  # noqa: F401
 from . import annotations  # noqa: F401
+from . import dfes  # noqa: F401
