771d4264147e5eab94dce3546765e0b01597a8af
silastittes <silas.tittes@gmail.com>
2024-03-11 16:06:29 -0700

    updates to hash codes and file names

--- stdpopsim/catalog/DroMel/annotations.py
+++ stdpopsim/catalog/DroMel/annotations.py
@@ -14,9 +14,9 @@ _an = stdpopsim.Annotation(
     gff_sha256="d882d9a2af1c090ad69b4c81e54b809506f7a8d5fdd90597c6ed05c79ad502bc",
     intervals_url=(
         "https://stdpopsim.s3-us-west-2.amazonaws.com/"
-        "annotations/DroMel/FlyBase_BDGP6.32.51_exons.tar.gz"
+        "annotations/DroMel/FlyBase_BDGP6.32.51_v1_exons.tar.gz"
     ),
-    intervals_sha256="665df7639725ca18bde85001bbdc08fbef4450c52a8788e02ba76206e2e03d50",
+    intervals_sha256="f3aa9eb288f76b1660cf30a38886f19139f8b15adc1210053bf9c7231167ac5c",
     citations=[
         stdpopsim.Citation(
             year=2014,
@@ -43,9 +43,9 @@ _an2 = stdpopsim.Annotation(
     gff_sha256="d882d9a2af1c090ad69b4c81e54b809506f7a8d5fdd90597c6ed05c79ad502bc",
     intervals_url=(
         "https://stdpopsim.s3-us-west-2.amazonaws.com/"
-        "annotations/DroMel/FlyBase_BDGP6.32.51_CDS.tar.gz"
+        "annotations/DroMel/FlyBase_BDGP6.32.51_v1_CDS.tar.gz"
     ),
-    intervals_sha256="3f84b69f7ca558f16cc9b787ee74ba5d16685cb80b2ba8a6ddca4b7b0c4486e0",
+    intervals_sha256="832befc80e2ba38ac9c84af164a3fc7089915c24225d37479707e1c7c098527e",
     citations=[
         stdpopsim.Citation(
             year=2014,
