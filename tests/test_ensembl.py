42836b8c01d96703aac0e8febcc1f90fbc42c465
silastittes <silas.tittes@gmail.com>
2024-03-15 08:43:14 -0700

    rfurther revisions to docs, reset ensemble version

--- tests/test_ensembl.py
+++ tests/test_ensembl.py
@@ -4,4 +4,4 @@ import stdpopsim
 # Make sure we don't update the release without realising it.
 def test_version():
     release = stdpopsim.catalog.ensembl_info.release
-    assert release == 111
+    assert release == 103
