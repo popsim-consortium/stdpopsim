bd7bab0b4c7e9c97a85f7064c485a8c5e940501a
peter <petrel.harp@gmail.com>
2024-12-23 14:35:12 -0800

    ssh

--- stdpopsim/slim_engine.py
+++ stdpopsim/slim_engine.py
@@ -1769,7 +1769,7 @@ class _SLiMEngine(stdpopsim.Engine):
         if verbosity is not None:
             slim_cmd.extend(["-d", f"verbosity={verbosity}"])
         print("FOO", script_file, os.path.isfile(script_file))
-        slim_cmd.append(script_file + "foofoofoo")
+        slim_cmd.append(script_file)
 
         with subprocess.Popen(
             slim_cmd,
