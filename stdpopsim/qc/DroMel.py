8eeec44a145555952560c0239a56f6142e782ef0
Clara Rehmann <crehmann@uoregon.edu>
2025-01-21 11:59:14 -0800

    qc

--- stdpopsim/qc/DroMel.py
+++ stdpopsim/qc/DroMel.py
@@ -115,3 +115,42 @@ def SheehanSongThreeEpic():
 
 
 _species.get_demographic_model("African3Epoch_1S16").register_qc(SheehanSongThreeEpic())
+
+
+def Gamma_H17():
+    id = "Gamma_H17"
+    description = "Deleterious Gamma DFE"
+    long_description = """
+    Return neutral and negative MutationType()s representing a drosophila DFE.
+    Huber et al. (2017), https://doi.org/10.1073/pnas.1619508114.
+    DFE parameters are based on the Full model described in Table S2, in which
+    singletons are excluded and a recent mutation rate estimate is used
+    (mu=3x10e-9, Keightley 2014).
+    """
+    citations = [
+        stdpopsim.Citation(
+            author="Huber et al.",
+            year=2017,
+            doi="https://doi.org/10.1073/pnas.1619508114",
+            reasons={stdpopsim.CiteReason.DFE},  # include the dfe_model reason
+        )
+    ]
+    neutral = stdpopsim.MutationType()
+    gamma_shape = 0.35
+    gamma_scale = 3.8e-04
+    gamma_mean = gamma_shape * gamma_scale
+    h = 0.5  # dominance coefficient
+    negative = stdpopsim.MutationType(
+        dominance_coeff=h,
+        distribution_type="g",  # gamma distribution
+        # (1+s for homozygote in SLiM versus 1+2s in dadi)
+        distribution_args=[-2 * gamma_mean, gamma_shape],
+    )
+    return stdpopsim.DFE(
+        id=id,
+        description=description,
+        long_description=long_description,
+        mutation_types=[neutral, negative],
+        proportions=[0.3, 0.7],
+        citations=citations,
+    )
