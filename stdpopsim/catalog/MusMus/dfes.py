5e63e81d546eba447cc6733979275130782a940a
Peter Ralph <petrel.harp@gmail.com>
2025-01-21 10:21:53 -0800

    include long description in DFE catalog entries (#1658)
    * rewrite of long descriptions
    
    * remove URLs from long descriptions

--- stdpopsim/catalog/MusMus/dfes.py
+++ stdpopsim/catalog/MusMus/dfes.py
@@ -13,19 +13,16 @@ def _BookerDFE():
     id = "Gamma_B21"
     description = "Deleterious Gamma DFE CDS"
     long_description = """
-    Return negative MutationType()s representing a Mus
-    musculus subsp. castaneous DFE for protein coded exons or CDS. Booker et al. (2021),
-    https://doi.org/10.1101/2021.06.10.447924
-    DFE parameters are based on an analysis of the unfolded site frequency spectrum
-    (uSFS) using polyDFE v2 (Tataru and Bataillon 2019) as presented in Booker
-    et al. (2021).
+    A gamma-distributed DFE for deleterious mutations estimated from the SFS of
+    Mus musculus castaneous exons by Booker et al. (2021), using polyDFE v2
+    (Tataru and Bataillon 2019).
     """
     citations = [
         stdpopsim.Citation(
             author="Booker et al.",
             year=2021,
             doi="https://doi.org/10.1101/2021.06.10.447924",
-            reasons={stdpopsim.CiteReason.DFE},  # include the dfe_model reason
+            reasons={stdpopsim.CiteReason.DFE},
         )
     ]
     neutral = stdpopsim.MutationType()
