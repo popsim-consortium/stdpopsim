1bddb254523a30fbcadf1883f2161f8e2c5fa9a9
andrewkern <adkern@uoregon.edu>
2025-01-30 14:40:32 -0800

    adding version in custom footer

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ except Exception:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
@@ -98,6 +99,14 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+html_context = {
+    "display_github": True,
+    "version": version,
+    "release": release,
+}
+html_theme_options = {
+    "version_selector": True,
+}
 
 # Show the version number in the navigation bar
 html_show_sphinx = True
@@ -107,6 +116,11 @@ html_show_version = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Add custom CSS to style the footer
+html_css_files = [
+    "custom.css",
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
