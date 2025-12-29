9f031cb457a54546092fc052f7ef75d59ddea5bd
jorgemasgomez <70472913+jorgemasgomez@users.noreply.github.com>
2025-12-29 17:20:31 +0100

    Update tests/test_PruDul.py
    Co-authored-by: Gregor Gorjanc <gregor.gorjanc@gmail.com>

--- tests/test_PruDul.py
+++ tests/test_PruDul.py
@@ -39,6 +39,7 @@ class TestGenomeData(test_species.GenomeTestBase):
     genome = stdpopsim.get_species("PruDul").genome
 
     # Recombination rates from linkage map in Mas-Gomez et al. (2025) Table 1
+    # and physical lengths from genome assembly in Castanera et al. (2024) - file genome_data.py
     # https://doi.org/10.1016/j.hpj.2025.04.013
     @pytest.mark.parametrize(
         ["name", "rate"],
