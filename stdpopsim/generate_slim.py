ccb30781a189129d69cdec72e2ef04ae25cb1961
jeff_spence <spence.jeffrey@berkeley.edu>
2026-06-30 13:30:26 -0700

    adding files to generate test scipt
    will remove them later because they don't belong here.

---
+++ stdpopsim/generate_slim.py
@@ -0,0 +1,59 @@
+import stdpopsim
+import numpy as np
+
+
+traits = [
+    stdpopsim.Trait(id="add1", type="additive"),
+    stdpopsim.Trait(id="add2", type="additive"),
+    stdpopsim.Trait(id="mult", type="multiplicative"),
+]
+tm = stdpopsim.TraitsModel(
+    traits=traits,
+)
+
+
+tm.add_environment(
+    id="env1",
+    trait_ids=["add1"],
+    distribution_type="mvn",
+    distribution_args=[np.zeros(1), np.eye(1)],
+    time_intervals=[(0, 10)],
+    population_list=['pop_0']
+)
+
+
+tm.add_environment(
+    id="env2",
+    trait_ids=["add1", "add2"],
+    distribution_type="mvn",
+    distribution_args=[np.zeros(2), np.eye(2)]
+)
+
+
+model = stdpopsim.PiecewiseConstantSize(10)
+engine = stdpopsim.get_engine("slim")
+species = stdpopsim.get_species("AnaPla")
+contig = species.get_contig("chr1", left=1e7, right=1e7 + 1e4)
+
+mt1 = stdpopsim.MutationType(
+    trait_ids=['add1', 'add2'],
+    distribution_type='mvn',
+    distribution_args=[np.zeros(2), np.eye(2)],
+)
+
+mt2 = stdpopsim.MutationType(
+    trait_ids=['mult'],
+    distribution_type='e',
+    distribution_args=[1.],
+)
+
+dme = stdpopsim.DistributionOfMutationEffects(
+    mutation_types=[mt1, mt2],
+    proportions=[0.3, 0.7]
+)
+contig.add_dme(intervals=np.array([[1e7, 1e7+1e4]]), DME=dme)
+
+ts = engine.simulate(
+    model, contig, samples={"pop_0": 3}, traits_model=tm, seed=7,
+    slim_script=True
+)
---
+++ stdpopsim/generate_slim.sh
@@ -0,0 +1,2 @@
+pip install --upgrade ..
+python generate_slim.py > test_script.slim
