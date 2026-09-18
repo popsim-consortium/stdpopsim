4d9543f3bd0b56ea3e55df341efea175bb2e8f65
Andrew Kern <adkern@uoregon.edu>
2026-09-18 15:30:07 -0700

    TestVersion

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,13 +1,16 @@
 """
-Tests for the package version.
+Test python package versioning
 """
 
 from packaging.version import Version
 
-import stdpopsim
+from stdpopsim import _version
 
 
-class TestVersion:
-    def test_version_is_pep440(self):
-        # A version that parses and round-trips unchanged is PEP 440 compliant.
-        assert str(Version(stdpopsim.__version__)) == stdpopsim.__version__
+class TestPythonVersion:
+    """
+    Test that the version is PEP440 compliant
+    """
+
+    def test_version(self):
+        assert str(Version(_version.version)) == _version.version
